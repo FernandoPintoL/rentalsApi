becbc0e4fb4fe5429a18ccc5c4fa9d66724a6bdd
Fernando Pinto Lino <30969689+FernandoPintoL@users.noreply.github.com>
2025-06-19 18:16:07 -0400

    update api

--- app/Http/Controllers/AuthenticatedController.php
+++ app/Http/Controllers/AuthenticatedController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Helpers\ApiResponse;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use App\Services\ResponseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class AuthenticatedController extends Controller
 {
@@ -28,31 +31,44 @@ class AuthenticatedController extends Controller
             if (Auth::attempt($credentials)) {
                 $user = User::where('email', $request->email)->first();
                 Auth::login($user);
-                if($user->hasRole('Propietario')){
-                    $user['role'] = "Propietario";
-                    // cargar datos del propietario
-                    $user['propietario'] = $user->propietario;
-                }
-                return ApiResponse::success([
-                    'user' => $user,
-                ], 'Inicio de sesión exitoso', 200);
+                $datosSession = session()->all();
+                $user->setAttribute('session_data', $datosSession);
+                return ResponseService::success('Inicio de sesión exitoso', $user,200);
             } else {
-                return ApiResponse::error('Las credenciales proporcionadas son incorrectas.', 401);
+                return ResponseService::error('Las credenciales proporcionadas son incorrectas.',[], 401);
             }
         } catch (\Exception $e) {
-            return ApiResponse::error('Se produjo un error durante el inicio de sesión.', 500, ['exception' => $e->getMessage()]);
+            return ResponseService::error('Se produjo un error durante el inicio de sesión.', $e->getMessage(), 500);
         }
     }
     public function createUser(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'usernick' => 'required|string|max:50',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'num_id' => 'required|string|max:20',
+            'tipo_usuario' => 'required',
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'usernick' => $request->usernick,
-            'password' => bcrypt($request->password),
+            'num_id' => $request->num_id,
+            'telefono' => $request->telefono,
+            'direccion' => $request->direccion,
+            'tipo_cliente' => $request->tipo_cliente,
+            'tipo_usuario' => $request->tipo_usuario,
+            'password' => Hash::make($request->password),
         ]);
 
-        return ApiResponse::success($user, 'Usuario creado exitosamente', 201);
+        Auth::login($user);
+        $datosSession = session()->all();
+        $user->setAttribute('session_data', $datosSession);
+
+        return ResponseService::success('Usuario creado exitosamente', $user,  201);
     }
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('tipo_usuario', ['propietario', 'cliente'])->default('cliente');
-            $table->enum('tipo_cliente', ['visitante', 'cliente'])->default('cliente');
+            $table->enum('tipo_cliente', ['normal','visitante', 'cliente'])->default('cliente');
             $table->string('direccion')->nullable();
             $table->rememberToken();
             $table->timestamps();
